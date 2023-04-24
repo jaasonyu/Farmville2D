07777c1ba818e0afe30deaca154e2bd04bc304db
Jason <jason_yu25@milton.edu>
2023-04-24 13:19:10 -0400

    added printPlot

--- src/MyFarm.java
+++ src/MyFarm.java
@@ -1,9 +1,10 @@
 public class MyFarm {
     public static void main(String[] args) {
         MyFarm charlie = new MyFarm();
-
     }
     public MyFarm(){
         System.out.println("Hello from my Farm!");
     }
+
 }
+
--- src/Plot.java
+++ src/Plot.java
@@ -13,8 +13,23 @@ public class Plot {
             plantName = "sunflower";
         }
         else if (num == 1){
-            plantName =="corn";
+            plantName ="corn";
         }
-
+        else if (num == 2){
+            plantName ="carrot";
+        }
+        else if (num == 3){
+            plantName ="tomato";
+        }
+        else if (num == 4){
+            plantName ="empty randomly";
+        }
     }
+    public void printPlot(){
+        System.out.println("the plot has " +numberOfPlant + " plants planted");
+        System.out.println("the plant needs water? " +needsWater);
+        System.out.println("the plot has " +plantName);
+    }
+
+
 }
