813910c290be8c81fa69c3ea3531efd67a1cc409
Jason <jason_yu25@milton.edu>
2023-05-04 20:24:08 -0400

    added numberOfTomatoPlots and numberOfEmptyPlots, having trouble figuring out how to have every other plot be true for needsWater

--- src/MyFarm.java
+++ src/MyFarm.java
@@ -23,6 +23,9 @@ public class MyFarm {
         printPlantNames();
         totalCarrots();
         averageNumberOfPlants();
+        numberOfTomatoPlots();
+        numberOfEmptyPlots();
+        everyOtherNeedsWater();
     }
 
     public void totalPlants() {
@@ -49,20 +52,59 @@ public class MyFarm {
         for (int h = 0; h < grid.length; h++) {
             for (int k = 0; k < grid[h].length; k++) {
                 if (grid[h][k].plantName.equals("carrot")) {
-                sumOfCarrots += grid[h][k].numberOfPlant;
+                    sumOfCarrots += grid[h][k].numberOfPlant;
                 }
             }
         }
         System.out.println("The total number of carrots is " + sumOfCarrots);
     }
+
     public void averageNumberOfPlants() {
         int average = 0;
-        for (int q = 0; q < grid.length; q++){
-            for (int o = 0; o < grid[q].length; o++){
-                average += grid[q][o].numberOfPlant/(grid[q].length*grid.length);
+        for (int q = 0; q < grid.length; q++) {
+            for (int o = 0; o < grid[q].length; o++) {
+                average += grid[q][o].numberOfPlant / (grid[q].length * grid.length);
             }
         }
         System.out.println("The average number of plants is " + average);
     }
+
+    public void numberOfTomatoPlots() {
+        int numberOfTomato = 0;
+        for (int y = 0; y < grid.length; y++) {
+            for (int w = 0; w < grid[y].length; w++) {
+                if (grid[y][w].plantName.equals("tomato")) {
+                    numberOfTomato += 1;
+                }
+            }
+        }
+        System.out.println("The total number of plot of tomatos are  " + numberOfTomato);
+    }
+    public void numberOfEmptyPlots() {
+        int numberOfEmpty = 0;
+        for (int e = 0; e < grid.length; e++) {
+            for (int f = 0; f < grid[e].length; f++) {
+                if (grid[e][f].plantName.equals("empty")) {
+                    numberOfEmpty += 1;
+                }
+            }
+        }
+        System.out.println("The total number of plot that are empty are " + numberOfEmpty);
+    }
+    public void everyOtherNeedsWater(){
+        boolean needsWater = false;
+        for (int b = 0; b < grid.length; b++) {
+            for (int v = 0; v < grid[b].length; v++) {
+                if (grid[b].length == 2) {
+                    needsWater = true;
+                }
+            }
+        }
+        System.out.println(needsWater + " the plants need water in this plot.");
+        // change the value of needsWater to be true for every other plot
+        // print the value or needs water for all plots row by row
+
+    }
+
 }
 
